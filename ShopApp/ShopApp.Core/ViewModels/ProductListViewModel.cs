baf198044cbec714700a92bdce02c28b8e0cb9dd
Bartosz Ciesielski <miketen1997@gmail.com>
2018-07-28 23:20:06 +0200

    Added basic filters

---
+++ ShopApp/ShopApp.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ShopApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Core.ViewModels
+{
+	public class ProductListViewModel
+	{
+		public IEnumerable<Product> Products { get; set; }
+		public IEnumerable<ProductCategory> ProductCategories { get; set; }
+	}
+}
